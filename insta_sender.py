67dfaa2a6ce51d4cb78ea1e0728c57fa98890720
Guilherme Gutirres de Castro <guilherme@gutirres.com>
2022-09-01 15:36:08 -0300

    Create insta_sender.py

---
+++ insta_sender.py
@@ -0,0 +1,7 @@
+#Postagem Instagram
+from instabot import Bot
+bot = Bot()
+bot.login(username = "",
+          password = "")
+bot.upload_photo("C:/Users/*.jpg",
+           caption = "Legenda da foto")
